6d0db0af9698547df13bc6e285a3ab49cc26a1fd
ROB301 <ROB301@ASRL-07>
2015-10-27 15:00:46 -0400

    New code to test

--- src/Lab4.java
+++ src/Lab4.java
@@ -14,7 +14,13 @@ public class Lab4 {
 	public static int prevLangle=0;
 	public static int prevRangle=0;
 	public static int BASESPEED = 100;
+	public static int MAX = 120;
+	public static int MIN = 80;
 	public static float P = 30;
+	public static double currangle = 0;
+	
+	public static float x=0;
+	public static float y=0;
 	
 	public static void main(String[] args) throws Exception{
 		
@@ -32,23 +38,63 @@ public class Lab4 {
 				
 			}
 			});
-		
-		getDistanceTravelled();
+		//turn(90);
+		setXY(200,20,5);
+		//setXY(100,100,180);
+		//setXY(0,100,-90);
+		//setXY(0,0,0);
+		/*getDistanceTravelled();
 		initWallFollow(100.0,20.0);
 		initWallFollow(100.0,30.0);
-		initWallFollow(100.0,20.0);
+		initWallFollow(100.0,20.0);*/
+		turn2(0);
+	}
+	public static void setXY(float xf, float yf,float thetaf){
+		
+		double turnangle = 180/Math.PI *Math.atan2(yf-y,xf-x);
+		turn(turnangle-currangle);
+		double r = Math.pow(Math.pow(yf-y,2) + Math.pow(xf-x, 2),0.5);
+		System.out.println(Double.toString(r));
+		moveForward(r);
+		y=yf;
+		x=xf;
+		turn(thetaf-currangle);
 	}
 	public static void turn(double degrees){
-		double r = L * degrees/(2*180.0*D);
-		int angle = (int)(r*360);
+		currangle += degrees;
+		double r = L * degrees/D;
+		System.out.println(Double.toString(r));
+		int angle = (int)(r);
+		Motor.A.setSpeed(100);
+		Motor.B.setSpeed(100);
+		Motor.A.rotate(-angle,true);
+		Motor.B.rotate(angle,true);
+		while(Motor.A.isMoving()){
+			Thread.yield();
+		}
+	}
+	public static void turn2(double degrees){
+		currangle += degrees;
+		double r = L * degrees/D;
+		System.out.println(Double.toString(r));
+		int angle = 5*360;
+		Motor.A.setSpeed(100);
+		Motor.B.setSpeed(147);
 		Motor.A.rotate(angle,true);
-		Motor.B.rotate(-angle,true);
-		
+		Motor.B.rotate((int) (1.47*angle),true);
+		while(Motor.A.isMoving()){
+			Thread.yield();
+		}
 	}
 	public static void moveForward(double meters){
+		Motor.A.setSpeed(360);
+		Motor.B.setSpeed(360);
 		double rotation = 360*meters/(Math.PI*D);
 		Motor.A.rotate((int)rotation,true);
 		Motor.B.rotate((int)rotation,true);
+		while(Motor.A.isMoving()){
+			Thread.yield();
+		}
 	}
 	public static float getWallDistance(){
 		int sampleSize = sonic.sampleSize();
@@ -62,10 +108,16 @@ public class Lab4 {
 		double remDist = dist;
 		while (remDist > 0){
 			 double error = getWallDistance() - targetWallDist;
+			 error *=P;
+			 if (error>20){
+				 error = 20;
+			 }else if(error < -20){
+				 error = -20;
+			 }
 			 Motor.A.forward();
 			 Motor.B.forward();
-			 Motor.A.setSpeed(BASESPEED-(int)(error*P));
-			 Motor.B.setSpeed(BASESPEED+(int)(error*P));
+			 Motor.A.setSpeed(BASESPEED-(int)(error));
+			 Motor.B.setSpeed(BASESPEED+(int)(error));
 			 remDist -= getDistanceTravelled();
 		}
 	}
